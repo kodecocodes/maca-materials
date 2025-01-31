44c03e6c24e5fdd15bcc7d66cab66749882d0320
mataharimau <mataharimau@users.noreply.github.com>
2025-01-31 12:56:54 +1100

    Corrected project

--- 01-introducing-xcode/projects/final/MyFirst/MyFirst/ContentView.swift
+++ 01-introducing-xcode/projects/final/MyFirst/MyFirst/ContentView.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2023 Kodeco LLC
+/// Copyright (c) 2025 Kodeco Inc.
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -37,15 +37,13 @@ struct ContentView: View {
     VStack {
       Image(systemName: "globe")
         .imageScale(.large)
-        .foregroundColor(.accentColor)
+        .foregroundStyle(.tint)
       ByeView()
     }
     .padding()
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+#Preview {
+  ContentView()
 }
