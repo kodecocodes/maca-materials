e3e13dfc023b4a4557c0ef90cf44e7ba75a00a01
Richard Critz <rcritz@gmail.com>
2026-04-05 16:08:26 -0400

    section 3 FPE

--- 16-using-cocoa-bindings/projects/final/MovieTables/MovieTables/ViewController.swift
+++ 16-using-cocoa-bindings/projects/final/MovieTables/MovieTables/ViewController.swift
@@ -98,8 +98,7 @@ class ViewController: NSViewController {
     }
 
     if let sortedMovies = (visibleMovies as NSArray)
-      .sortedArray(using: moviesTableView.sortDescriptors) as? [Movie]
-    {
+      .sortedArray(using: moviesTableView.sortDescriptors) as? [Movie] {
       visibleMovies = sortedMovies
     }
     moviesTableView.reloadData()
